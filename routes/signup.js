810e9d8eb6086c89ca077e51eebc53d7d8c0a6e2
shorner102 <sydnihorner@gmail.com>
2017-05-08 21:05:33 -0400

    signing up as a user

--- routes/signup.js
+++ routes/signup.js
@@ -1,10 +1,24 @@
 const express = require('express');
-const passport = require('passport');
+var bcrypt = require('bcrypt');
 const router = express.Router();
+const data = require("../data");
+const userData = data.user;
 
 router.get('/', function(req, res){
   res.render("signup.handlebars");
   
 });
 
+router.post("/",  function(req, res){
+
+  bcrypt.hash(req.body.pwd1, 10, function(err, hash) {
+        // Store hash in your password DB.
+    
+     userData.addUser(req.body.firstName, req.body.lastName, req.body.email, hash, req.body.location, req.body.skills, req.body.experience, req.body.field)
+     .then((user)=>{
+       res.redirect("/private");
+       
+     });
+  });
+});
 module.exports = router;
